d93a3bb954af5b296a65e927085ea2c3d233379d
Sameer <56549288+sameersharma1999@users.noreply.github.com>
2021-03-03 09:37:58 +0530

    Updated

--- StockPicker.py
+++ StockPicker.py
@@ -11,19 +11,6 @@ def StockPicker(arr):
 		return -1
 	else:
 		return max_diff
-
-
-	# for i in range(0, n-1):
-	# 	for j in range(0, n-i-1):
-	# 		diff = j-i
-	# 		if diff > max_diff:
-	# 			max_diff = diff
-	# if max_diff == 0:
-	# 	return -1
-	# else:
-	# 	return max_diff
-
-
-
+	
 arr = [14,20,4,12,5,11]
 print(StockPicker(arr))
